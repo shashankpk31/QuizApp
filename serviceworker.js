17ed6751f83157902792765377a20ee687e81aa7
shashankpk31 <shashankpk34@gmail.com>
2021-09-10 12:20:31 +0530

    Updates

---
+++ serviceworker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'VERSION-1';
+const urlsToCache = ['index.html', 'offline.html'];
+const self = this;
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    )
+})
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(() => {
+                return fetch(e.request)
+                .catch(() => caches.match('offline.html'))
+            })
+    )
+})
+
+self.addEventListener('activate', (e) => {
+      const cacheWhitelist=[];
+      cacheWhitelist.push(CACHE_NAME);
+      e.waitUntil(
+          caches.keys().then((cacheNames)=>Promise.all(
+              cacheNames.map((cacheName)=>{
+                  if (!cacheWhitelist.includes(cacheName)) {
+                      return caches.delete(cacheName)
+                  }
+              })
+          ))
+      )
+})
